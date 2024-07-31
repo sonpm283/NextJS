5dbfb77e8d1474ce655bd56c56f21ae0aa10eb78
PhanMinhSon <sonpm@hivelab.com>
2024-07-31 16:57:39 +0700

    [shadcn] feat: Dark Mode Toggle

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,27 @@
 import type { Metadata } from "next";
-// import { Inter } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
-import localFont from "next/font/local";
+import { ThemeProvider } from "@/components/theme-provider";
+// import localFont from "next/font/local";
 
-// const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] });
 
-const myFont = localFont({
-  src: [
-    {
-      path: "./Roboto-Regular.ttf",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "./Roboto-Thin.ttf",
-      weight: "100",
-      style: "normal",
-    },
-  ],
+// const myFont = localFont({
+//   src: [
+//     {
+//       path: "./Roboto-Regular.ttf",
+//       weight: "400",
+//       style: "normal",
+//     },
+//     {
+//       path: "./Roboto-Thin.ttf",
+//       weight: "100",
+//       style: "normal",
+//     },
+//   ],
 
-  variable: "--font-roboto",
-});
+//   variable: "--font-roboto",
+// });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -33,11 +34,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${myFont.className} ${myFont.variable}`}>
-        <header className="bg-slate-200">Page Header</header>
-        {children}
-        <footer className="bg-slate-200">Page Footer</footer>
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${inter.className}`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {/* <header className="bg-slate-200">Page Header</header> */}
+          {children}
+          {/* <footer className="bg-slate-200">Page Footer</footer> */}
+        </ThemeProvider>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,9 +10,10 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import Card from "./Card";
+// import Card from "./Card";
 import Link from "next/link";
-import ButtonRedirect from "./components/ButtonRedirect";
+import { ModeToggle } from "@/components/mode-toggle";
+// import ButtonRedirect from "./components/ButtonRedirect";
 
 export default function Home() {
   return (
@@ -40,6 +41,7 @@ export default function Home() {
             />
           </a>
         </div>
+        <ModeToggle />
       </div>
       <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
         <Image
@@ -67,7 +69,7 @@ export default function Home() {
           </DialogHeader>
         </DialogContent>
       </Dialog>
-      <Card />
+      {/* <Card /> */}
       <h1 className="text-2xl font-thin">Roboto Thin</h1>
       <h1 className="text-2xl font-normal font-roboto">Roboto Regular</h1>
       <a href="#none" className="text-3xl font-roboto">
@@ -78,7 +80,7 @@ export default function Home() {
       <Link href="/register">Register Link</Link>
       <Link href="login">Login Link</Link>
 
-      <ButtonRedirect />
+      {/* <ButtonRedirect /> */}
     </main>
   );
 }
